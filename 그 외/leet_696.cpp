cae75742eaa4c3a850030077b29c480ed31cfdd3
canis617 <whwhdgud96@gmail.com>
2020-04-16 18:06:53 +0900

    leet_696_start

---
+++ 그 외/leet_696.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        char* p1;
+        char* p2;
+        p1 = &s[0];
+        p2 = &s[0];
+        p2++;
+        int p_count = 1;
+        int n_count = 0;
+    
+        while (*(p2-1) != s[s.length-1]){
+            if (*p1 == *p2) {
+                p_count++;
+
+            }
+            else{
+
+            }
+        }
+    }
+};
